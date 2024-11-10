fc1427b9179ec71e39ffe6862a0229683ca645ff
Marko Jerkić <markojerkic266@gmail.com>
2024-11-10 18:46:08 +0100

    igranje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  injectQuery,
+  injectQueryClient,
+} from '@tanstack/angular-query-experimental';
+import { findPetsByStatus } from '../client';
+import {
+  getPetByIdOptions,
+  getPetByIdQueryKey,
+} from '../client/@tanstack/angular-query-experimental.gen';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [JsonPipe],
+  template: `
+    @if (data.data(); as pet) {
+      <pre>{{ pet | json }}</pre>
+    }
+
+    <button (click)="revalidate()">Refetch</button>
+  `,
+
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'open-api-angular-tanstack';
+
+  public queryClient = injectQueryClient();
+
+  private petId = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
+  public data = injectQuery(() => ({
+    ...getPetByIdOptions({
+      path: {
+        petId: this.petId,
+      },
+    }),
+  }));
+
+  public revalidate() {
+    const qk = getPetByIdQueryKey({
+      path: {
+        petId: this.petId,
+      },
+    });
+    this.queryClient.invalidateQueries({
+      queryKey: qk,
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,27 @@ import { routes } from './app.routes';
 import {
   provideTanStackQuery,
   QueryClient,
+  withDevtools,
 } from '@tanstack/angular-query-experimental';
+import { client } from '../client';
+
+client.setConfig({
+  // set default base url for requests made by this client
+  baseUrl: 'https://petstore3.swagger.io/api/v3',
+  /**
+   * Set default headers only for requests made by this client. This is to
+   * demonstrate local clients and their configuration taking precedence over
+   * internal service client.
+   */
+  headers: {
+    Authorization: 'Bearer <token_from_local_client>',
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideTanStackQuery(new QueryClient()),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
   ],
 };
